c582024cd983b6ecb27080ef4949ce1fef16fc67
ziinspiration <ramddbgk@gmail.com>
2024-09-28 10:16:14 +0700

    Tugas 1

---
+++ src/Sesi1/HelloWorldSwing.java
@@ -0,0 +1,26 @@
+package Sesi1;
+
+import javax.swing.*;
+
+public class HelloWorldSwing {
+
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello, Ilham.");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
